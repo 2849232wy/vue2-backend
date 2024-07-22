4d5888261c8712f3295707b6bd3def0f128dadd7
Your Name <279761649@qq.com>
2024-07-22 18:05:30 +0800

    略微修改

--- src/views/PagesTwo/index.vue
+++ src/views/PagesTwo/index.vue
@@ -1,15 +1,11 @@
 <template>
-  <div>
-
-  </div>
+  <div></div>
 </template>
 
 <script>
   export default {
-    
+    import { onMounted } from "Vue"
   }
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
